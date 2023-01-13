a2f69abcdded0903be612000f23d905b36c3aa89
loren <lorenmichaelh@gmail.com>
2023-01-13 11:57:32 -0600

    update movie serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,7 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :poster_url, :mpaa, :year, :runtime, :synopsis, :availability
+  attributes :id, :title, :poster_url, :mpaa, :year, :runtime, :synopsis, :summary, :availability, :store_id
+
+  def summary
+    "#{self.object.synopsis[0..49]}"
+  end
 end
