f2cd788840aac9f8e7ffac7b8120e86d1d8bdc6e
loren <lorenmichaelh@gmail.com>
2023-01-13 11:40:44 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  # root "articles#index"
+    
+  get '/me', to: "users#show"
+  post '/signup', to: "users#create"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
   resources :rentals
   resources :users
   resources :stores
   resources :movies
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  namespace :api do
-    get '/hello', to: 'application#hello_world'
-  end
-
+  # namespace :api do
+  #   get '/hello', to: 'application#hello_world'
+  # end
 
   get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
