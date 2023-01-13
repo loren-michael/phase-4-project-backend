a62fee6019ebe74a67ab61e5d336e2404866dd3d
loren <lorenmichaelh@gmail.com>
2023-01-13 11:55:18 -0600

    add actions to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordInvalid, with: :render_422
+  before_action :authorize
+  # def hello_world
+  #   session[:count] = (session[:count] || 0) + 1
+  #   render json: { count: session[:count] }
+  # end
 
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: { count: session[:count] }
+  def logged_in?
+    !!session[:user_id]
   end
+
+  def current_user
+    @current_user = User.find_by_id(session[:user_id]) if logged_in?
+  end
+
+  private
+
+    def authorize
+      @current_user = User.find_by(id: session[:user_id])
+      render json: { errors: ["User not authorized."] }, status: :unauthorized
+    end
+
+    def render_422(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: 422
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render json: @user, status: 201
+    else
+      render json: { errors: @user.errors.full_messages }, status: 422
+    end
+  end
+
+
+  def show
+    render json: @current_user
+  end
+
+  private
+
+    def user_params
+      params.permit(:username, :password, :password_confirmation)
+    end
+
 end
