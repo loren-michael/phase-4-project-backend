be3859ac19d2fb01311179b1ad5cce6dcc3fc200
loren <lorenmichaelh@gmail.com>
2023-01-18 11:14:46 -0600

    add stores container code

--- client/src/components/StoresContainer.js
+++ client/src/components/StoresContainer.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function StoresContainer() {
+function StoresContainer({ stores }) {
   return (
-    <div>StoresContainer</div>
+    
+    <div>
+      <br></br>
+      Choose a location
+      {stores.map(store => {
+        return <li key={store.address}>{store.address}</li>
+      })}
+    </div>
   )
 }
 
