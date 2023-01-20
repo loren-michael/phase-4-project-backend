d1d139780b6d42b34d4a383409cc77b8d47f1b83
loren <lorenmichaelh@gmail.com>
2023-01-19 20:17:09 -0600

    add filter to MovieContainer

--- client/src/components/MoviesContainer.js
+++ client/src/components/MoviesContainer.js
@@ -1,15 +1,43 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
-import MovieCard from './MovieCard'
+import '../styles.css';
+import React, { useEffect, useState } from 'react';
+import { Card } from 'semantic-ui-react';
+import MovieCard from './MovieCard';
 
 function MoviesContainer({ movies }) {
+  const [filtered, setFiltered] = useState(false)
+  const [displayMovies, setDisplayMovies] = useState(movies)
+  const [filteredMovies, setFilteredMovies] = useState([])
+
+  function handleFilterToggle() {
+    setFiltered(!filtered)
+    filtered ? setDisplayMovies(movies) : setDisplayMovies(filteredMovies)
+  }
+
+  useEffect(() => {
+    const availableMovies = [];
+    movies.map((movie) => {
+      if (movie.availability) {
+        availableMovies.push(movie)
+      }
+    })
+    setFilteredMovies(availableMovies)
+  }, [])
+
   return (
-    <Card.Group>
-      {movies.map(movie => {
-      return (<MovieCard key={movie.id} movie={movie} />)
-    })}
-    </Card.Group>
-    
+    <>
+    <br></br>
+      <button onClick={handleFilterToggle}>{filtered ? "Show all movies" : "Show available movies"}</button>
+      <br></br>
+      <br></br>
+      <Card.Group className="movie-cards">
+        {displayMovies.map(movie => {
+          return (<MovieCard key={movie.id} movie={movie} />)
+        })}
+      </Card.Group>
+      {/* <Card.Group className="movie-cards">
+        {filtered ? <div>Available movies</div> : <div>All movies</div>}
+      </Card.Group> */}
+    </>
   )
 }
 
