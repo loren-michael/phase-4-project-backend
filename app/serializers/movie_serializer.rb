dce707aff51993965cf5caaf87c41bec31cd2e2e
loren <lorenmichaelh@gmail.com>
2023-01-23 16:19:43 -0600

    add rentals to user serializer, stores to movie serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :poster_url, :mpaa, :year, :runtime, :synopsis, :summary, :availability, :store_id
+  attributes :id, :title, :poster_url, :mpaa, :year, :runtime, :synopsis, :summary, :availability, :store_id, :store
 
   def summary
     "#{self.object.synopsis[0..49]}"
