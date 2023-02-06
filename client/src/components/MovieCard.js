3999fde3798ae7ad9f38b0de1fc94dcddcb45ce5
loren <lorenmichaelh@gmail.com>
2023-02-06 13:02:40 -0600

    add moviecard summary ternary

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -4,6 +4,9 @@ import { Card, Image } from 'semantic-ui-react';
 import MovieDetails from './MovieDetails';
 
 function MovieCard({ movie }) {
+
+  const movieSummary = movie.synopsis.substring(0,79)
+
   return (
     <Card color={ movie.availability ? "green" : "red" } href={`/movies/${movie.id}`}>
       <Link to={`/movies/${movie.id}`} element={<MovieDetails movie={movie} />}>
@@ -11,7 +14,7 @@ function MovieCard({ movie }) {
       <Card.Content>
         <Card.Header>{movie.title}</Card.Header>
         <div>{movie.year}</div>
-        <Card.Description>{movie.summary}...</Card.Description>
+        <Card.Description>{movie.summary ? movie.summary : movieSummary}...</Card.Description>
       </Card.Content>
       </Link>
     </Card>
