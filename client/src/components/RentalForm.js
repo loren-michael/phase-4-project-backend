f3890b99b3ce78cef3e61d2bd78b9fc6e34736af
loren <lorenmichaelh@gmail.com>
2023-02-06 12:06:38 -0600

    cleanup rentalform component

--- client/src/components/RentalForm.js
+++ client/src/components/RentalForm.js
@@ -34,8 +34,6 @@ function RentalForm({ user, movies, setMovies, activeRentals, setActiveRentals, 
 
   function handleReturn(e) {
     const rentalId = parseInt(e.target.value)
-    // updateMovieAvailabilityTrue(rentalId)
-    // DELETE /rentals
     fetch(`/rentals/${rentalId}`, {
       method: "DELETE",
       headers: {
@@ -43,28 +41,19 @@ function RentalForm({ user, movies, setMovies, activeRentals, setActiveRentals, 
         "accept": "application/json"
       }
     })
-    .then(r => r.json())
-    .then(rentals => setActiveRentals(rentals))
-    .then(updateMovieAvailabilityTrue(rentalId))
+    .then(r => {
+      if (r.ok) {
+        r.json()
+        .then(rentals => setActiveRentals(rentals))
+        .then(updateMovieAvailabilityTrue(rentalId))
+      } else {
+        r.json().then(data => setErrors(data.errors))
+      }
+    })
   }
 
 
   function handleRentalStart(e) {
-    // console.log("handle rental start")    
-    // console.log(rental)
-    // const userRentalJoinTable = user.rentals
-    // console.log(userRentalJoinTable)
-    // const rentedMovies = userRentalJoinTable.forEach(rental => {
-    //   movies.map(movie => {
-    //     if (movie.id === rental.movie_id) {
-    //       return movie
-    //     } else {
-    //       return null
-    //     }
-    //   })
-    // })
-    // // console.log(rentedMovies)
-    // POST a new rental
     fetch(`/rentals`, {
       method: "POST",
       headers: {
@@ -84,12 +73,8 @@ function RentalForm({ user, movies, setMovies, activeRentals, setActiveRentals, 
 
 
   function patchMovieAvailability() {
-    console.log("patch movie availability")
-    // ONLY DO THESE THINGS IF THE RENTAL IS SUCCESSFUL!
-    // set rental movie's availbility to false to map into movies state
     const updatedAvailability = rentalMovie
     updatedAvailability.availability = false
-    // map through movies and replace movie with updated availability record
     const newRentalMovies = movies.map(movie => {
       if (movie.id === updatedAvailability.id) {
         return updatedAvailability
@@ -97,35 +82,26 @@ function RentalForm({ user, movies, setMovies, activeRentals, setActiveRentals, 
         return movie
       }
     })
-    // set the movies state to reflect updated availability on all pages
     setMovies(newRentalMovies)
-    // add the same rental info to the active rentals state in the correct format
-    // const updatedActiveRentals = activeRentals
     const newActiveRentalMovie = {movie: rentalMovie}
     const updatedActiveRentals = [...activeRentals, newActiveRentalMovie]
-    // updatedActiveRentals.push(newActiveRentalMovie)
     setActiveRentals(updatedActiveRentals)
-    // change the rental button to either disappear or turn into a link back to choose a movie
     setRentalMovie({})
   }
 
   function updateMovieAvailabilityTrue(id) {
-    // find movie that's being returned to update availability of the object
-    console.log(id)
-
-    const foundMovie = activeRentals.find(rental => rental.id === id)
-    // update availability in foundmovie object
-    // PATCH /movies/:id after rental delete
-    console.log(foundMovie)
-
+    const foundRental = activeRentals.find(rental => rental.id === id)
+    const foundMovie = foundRental.movie
+    foundMovie.availability = true
+    const newMovies = movies.map(movie => {
+      if (movie.id === foundMovie.id) {
+        return foundMovie
+      } else {
+        return movie
+      }
+    })
+    setMovies(newMovies)
   }
-
-  function handleReturnStart(e) {
-    // DELETE rental from database
-    console.log(e.target.value)
-    // fetch(`/rentals/`)
-  }
-
 
 
   return (
@@ -136,14 +112,13 @@ function RentalForm({ user, movies, setMovies, activeRentals, setActiveRentals, 
       <ul>
         {activeRentals.map(rental => {
           return (
-            <li key={rental.movie.id} value={rental.id}> {rental.movie.title} <button value={rental.id} id={rental.id} onClick={e => handleReturnStart(e)}>Return this movie</button></li>
+            <li key={rental.movie.id} value={rental.id}> {rental.movie.title} <button value={rental.id} id={rental.id} onClick={e => handleReturn(e)}>Return this movie</button></li>
           )
         })}
       </ul>
       <br></br>
-      {/* { returnMovie.title ? <div>Confirm return of returnMovie.title <button> Yes, Return this Rental </button></div> : <div></div> }  */}
       
-      { rentalMovie.title ? <div><h4>Begin rental of {rentalMovie.title}</h4><button value={rentalMovie.movie_id} onClick={handleRentalStart} >Activate Rental</button></div> : <Link to="/movies">Choose a movie to rent</Link> }
+      { rentalMovie.title ? <div><h4>Begin rental of {rentalMovie.title}</h4><button value={rentalMovie.movie_id} onClick={e => handleRentalStart(e)} >Activate Rental</button></div> : <Link to="/movies">Choose a movie to rent</Link> }
       
       {errors.map(err => {
         return (
