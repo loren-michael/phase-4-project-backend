79b2ed671df7609b766afeb3a3c7ddd2652285a3
loren <lorenmichaelh@gmail.com>
2023-02-13 14:30:49 -0600

    add uniqueness validation to rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,7 +1,7 @@
 class Rental < ApplicationRecord
   validates :user_id, presence: true
   validates :store_id, presence: true
-  validates :movie_id, presence: true
+  validates :movie_id, presence: true, uniqueness: true
   belongs_to :user
   belongs_to :store
   belongs_to :movie
